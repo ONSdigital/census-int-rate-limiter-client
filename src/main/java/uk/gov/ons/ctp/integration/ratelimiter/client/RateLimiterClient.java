4ef20f23288678683938d681772f78326808c843
Peter Bochel <Peter.Bochel@ext.ons.gov.uk>
2020-10-21 10:15:11 +0100

    CR-1279 Added enum for domain name

--- src/main/java/uk/gov/ons/ctp/integration/ratelimiter/client/RateLimiterClient.java
+++ src/main/java/uk/gov/ons/ctp/integration/ratelimiter/client/RateLimiterClient.java
@@ -1,8 +1,8 @@
 package uk.gov.ons.ctp.integration.ratelimiter.client;
 
-import java.util.Optional;
 import com.godaddy.logging.Logger;
 import com.godaddy.logging.LoggerFactory;
+import java.util.Optional;
 import uk.gov.ons.ctp.common.domain.CaseType;
 import uk.gov.ons.ctp.common.domain.UniquePropertyReferenceNumber;
 import uk.gov.ons.ctp.common.rest.RestClient;
@@ -15,33 +15,44 @@ public class RateLimiterClient {
 
   private RestClient rateLimiterClient;
 
+  public enum Domain {
+    RHSvc("respondenthome");
+
+    private String domainName;
+
+    private Domain(String domainName) {
+      this.domainName = domainName;
+    }
+  }
+
   public RateLimiterClient(RestClient rateLimiterClient) {
     super();
     this.rateLimiterClient = rateLimiterClient;
   }
 
-  public RateLimitResponse checkRateLimit(String domain, Product product,
+  public RateLimitResponse checkRateLimit(
+      Domain domain,
+      Product product,
       CaseType caseType,
       String ipAddress,
       UniquePropertyReferenceNumber uprn,
       Optional<String> telNo) {
-    log.with("domain", domain)
-        .debug("checkRateLimit() calling Rate Limiter Service");
+    log.with("domain", domain.domainName).debug("checkRateLimit() calling Rate Limiter Service");
 
-//    RateLimitResponse rateLimitResponse = null;
-//    try {
-//      rateLimitResponse =
-//          rateLimiterClient.postResource(
-//              RATE_LIMITER_QUERY_PATH, rateLimitRequest, RateLimitResponse.class);
-//    } catch (ResponseStatusException ex) {
-//      if (ex.getStatus() == HttpStatus.TOO_MANY_REQUESTS) {
-//        log.info("Rate limit exceeded");
-//      }
-//      throw ex;
-//    }
-//
-//    return rateLimitResponse;
-    
+    //    RateLimitResponse rateLimitResponse = null;
+    //    try {
+    //      rateLimitResponse =
+    //          rateLimiterClient.postResource(
+    //              RATE_LIMITER_QUERY_PATH, rateLimitRequest, RateLimitResponse.class);
+    //    } catch (ResponseStatusException ex) {
+    //      if (ex.getStatus() == HttpStatus.TOO_MANY_REQUESTS) {
+    //        log.info("Rate limit exceeded");
+    //      }
+    //      throw ex;
+    //    }
+    //
+    //    return rateLimitResponse;
+
     return null;
   }
 }
--- src/test/java/uk/gov/ons/ctp/integration/ratelimiterclient/RateLimiterClientTest.java
+++ src/test/java/uk/gov/ons/ctp/integration/ratelimiterclient/RateLimiterClientTest.java
@@ -1,14 +1,15 @@
 package uk.gov.ons.ctp.integration.ratelimiterclient;
 
 import static org.junit.Assert.assertEquals;
-import java.util.HashMap;
-import java.util.Map;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import uk.gov.ons.ctp.common.rest.RestClient;
 import uk.gov.ons.ctp.common.rest.RestClientConfig;
 import uk.gov.ons.ctp.integration.ratelimiter.model.RateLimitRequest;
@@ -16,90 +17,91 @@ import uk.gov.ons.ctp.integration.ratelimiter.model.RateLimitResponse;
 
 public class RateLimiterClientTest {
   private ObjectMapper objectMapper;
-  
+
   private RestClient restClient;
-  
-  
+
   public static void main(String[] args) throws JsonMappingException, JsonProcessingException {
 
     RateLimiterClientTest rateLimiterClientTest = new RateLimiterClientTest();
 
     rateLimiterClientTest.setLimit(false);
     rateLimiterClientTest.callJsonEndpoint("200");
-    
+
     rateLimiterClientTest.setLimit(true);
     rateLimiterClientTest.callJsonEndpoint("200");
   }
-  
+
   private void setLimit(boolean tooManyRequests) {
     Map<String, String> headerParams = new HashMap<>();
-    
+
     MultiValueMap<String, String> queryParams = new LinkedMultiValueMap<>();
     queryParams.add("enabled", Boolean.toString(tooManyRequests));
-    
-    String response = restClient.postResource("limit", null, String.class, headerParams, queryParams, "");
+
+    String response =
+        restClient.postResource("limit", null, String.class, headerParams, queryParams, "");
     System.out.println(response);
-    
   }
 
   public RateLimiterClientTest() {
     this.objectMapper = new ObjectMapper();
     objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
-    
+
     RestClientConfig restClientConfig = new RestClientConfig("http", "localhost", "8181", "", "");
     this.restClient = new RestClient(restClientConfig);
   }
-  
+
   private void callJsonEndpoint(String expectedHttpStatus) throws JsonProcessingException {
-    
+
     RateLimitRequest req = createRateLimitRequest();
-    
+
     RateLimitResponse response = restClient.postResource("json", req, RateLimitResponse.class, "");
     System.out.println(convertToJson(response));
-    
+
     String actualHttpStatus = response.getOverallCode();
     assertEquals(expectedHttpStatus, actualHttpStatus);
   }
-  
-  private RateLimitRequest createRateLimitRequest() throws JsonMappingException, JsonProcessingException {
-    String json = "{\n" + 
-        "  \"domain\": \"respondenthome\",\n" + 
-        "  \"descriptors\": [\n" + 
-        "    {\n" + 
-        "      \"entries\": [\n" + 
-        "        {\"key\": \"productGroup\", \"value\":  \"UAC\"},\n" + 
-        "        {\"key\": \"individual\", \"value\":  \"false\"},\n" + 
-        "        {\"key\": \"deliveryChannel\", \"value\":  \"SMS\"},\n" + 
-        "        {\"key\": \"caseType\", \"value\":  \"HH\"},\n" + 
-        "        {\"key\": \"uprn\", \"value\":  \"987\"}\n" + 
-        "      ]\n" + 
-        "    },\n" + 
-        "    {\n" + 
-        "      \"entries\": [\n" + 
-        "        {\"key\": \"productGroup\", \"value\":  \"UAC\"},\n" + 
-        "        {\"key\": \"individual\", \"value\":  \"false\"},\n" + 
-        "        {\"key\": \"deliveryChannel\", \"value\":  \"SMS\"},\n" + 
-        "        {\"key\": \"caseType\", \"value\":  \"HH\"},\n" + 
-        "        {\"key\": \"telNo\", \"value\":  \"07968583119\"}\n" + 
-        "      ]\n" + 
-        "    },\n" + 
-        "    {\n" + 
-        "      \"entries\": [\n" + 
-        "        {\"key\": \"productGroup\", \"value\":  \"UAC\"},\n" + 
-        "        {\"key\": \"individual\", \"value\":  \"false\"},\n" + 
-        "        {\"key\": \"deliveryChannel\", \"value\":  \"SMS\"},\n" + 
-        "        {\"key\": \"caseType\", \"value\":  \"HH\"},\n" + 
-        "        {\"key\": \"ipAddress\", \"value\":  \"123.123.123.123\"}\n" + 
-        "      ]\n" + 
-        "    }\n" + 
-        "  ]\n" + 
-        "}";
-    
+
+  private RateLimitRequest createRateLimitRequest()
+      throws JsonMappingException, JsonProcessingException {
+    String json =
+        "{\n"
+            + "  \"domain\": \"respondenthome\",\n"
+            + "  \"descriptors\": [\n"
+            + "    {\n"
+            + "      \"entries\": [\n"
+            + "        {\"key\": \"productGroup\", \"value\":  \"UAC\"},\n"
+            + "        {\"key\": \"individual\", \"value\":  \"false\"},\n"
+            + "        {\"key\": \"deliveryChannel\", \"value\":  \"SMS\"},\n"
+            + "        {\"key\": \"caseType\", \"value\":  \"HH\"},\n"
+            + "        {\"key\": \"uprn\", \"value\":  \"987\"}\n"
+            + "      ]\n"
+            + "    },\n"
+            + "    {\n"
+            + "      \"entries\": [\n"
+            + "        {\"key\": \"productGroup\", \"value\":  \"UAC\"},\n"
+            + "        {\"key\": \"individual\", \"value\":  \"false\"},\n"
+            + "        {\"key\": \"deliveryChannel\", \"value\":  \"SMS\"},\n"
+            + "        {\"key\": \"caseType\", \"value\":  \"HH\"},\n"
+            + "        {\"key\": \"telNo\", \"value\":  \"07968583119\"}\n"
+            + "      ]\n"
+            + "    },\n"
+            + "    {\n"
+            + "      \"entries\": [\n"
+            + "        {\"key\": \"productGroup\", \"value\":  \"UAC\"},\n"
+            + "        {\"key\": \"individual\", \"value\":  \"false\"},\n"
+            + "        {\"key\": \"deliveryChannel\", \"value\":  \"SMS\"},\n"
+            + "        {\"key\": \"caseType\", \"value\":  \"HH\"},\n"
+            + "        {\"key\": \"ipAddress\", \"value\":  \"123.123.123.123\"}\n"
+            + "      ]\n"
+            + "    }\n"
+            + "  ]\n"
+            + "}";
+
     RateLimitRequest request = objectMapper.readValue(json, RateLimitRequest.class);
-    
+
     return request;
   }
-  
+
   private String convertToJson(Object o) throws JsonProcessingException {
     return objectMapper.writeValueAsString(o);
   }
