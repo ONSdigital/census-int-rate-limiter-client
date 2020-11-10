434dd695478cfae7b79b4ccdf0adac7ec935c556
Peter Bochel <Peter.Bochel@ext.ons.gov.uk>
2020-11-10 09:20:53 +0000

    CR-1330 Renamed the manual test to be an integration test, so that it's not run by Maven

--- src/test/java/uk/gov/ons/ctp/integration/ratelimiterclient/RateLimiterClient_IT.java
+++ src/test/java/uk/gov/ons/ctp/integration/ratelimiterclient/RateLimiterClient_IT.java
@@ -31,7 +31,7 @@ import uk.gov.ons.ctp.integration.ratelimiter.model.RateLimitResponse;
  * environment variable is not set then the program doesn't do anything (and will therefore 'pass'
  * the test)
  */
-public class RateLimiterClientIntegrationTest {
+public class RateLimiterClient_IT {
   private String limiterHost;
 
   private RestClient restClient;
