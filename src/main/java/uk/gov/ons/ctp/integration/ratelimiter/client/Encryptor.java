1e0134caeca764bd93faf9d63aa06a9696d20b4e
Rob Catling <github@rpc-soft.com>
2021-01-07 15:13:09 +0000

    CR-1540 very simple start to this work

---
+++ src/main/java/uk/gov/ons/ctp/integration/ratelimiter/client/Encryptor.java
@@ -0,0 +1,9 @@
+package uk.gov.ons.ctp.integration.ratelimiter.client;
+
+public final class Encryptor {
+
+  static String aesEncrypt(String value, String password) {
+    // WRITEME
+    return null;
+  }
+}
---
+++ src/test/java/uk/gov/ons/ctp/integration/ratelimiterclient/EncryptorTest.java
@@ -0,0 +1,11 @@
+package uk.gov.ons.ctp.integration.ratelimiterclient;
+
+import org.junit.Test;
+
+public class EncryptorTest {
+
+  @Test
+  public void dummy() {}
+
+  // WRITEME
+}
